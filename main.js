59316274eafd434527e1aedaaeb94e2ff9665cdb
Steve Geluso <stevegeluso@gmail.com>
2016-12-12 17:03:06 -0800

    displaying random times on the clock on reload

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var hour = Math.floor(Math.random() * 12) + 1;
+  var minute = Math.floor(Math.random() * 60);
+
+  var hour10 = Math.floor(hour / 10);
+  var hour01 = hour % 10;
+  var minute10 = Math.floor(minute / 10);
+  var minute01 = minute % 10;
+
+  console.log(hour10, hour01, minute10, minute01);
+
   setBackgroundImage("background", 'background');
-  setHour10("hour10", '1');
-  setHour01("hour01", '1');
+  setHour10("hour10", hour10);
+  setHour01("hour01", hour01);
   setBackgroundImage("seperator", 'seperator');
-  setMinute10("minute10", '3');
-  setMinute01("minute01", '4');
+  setMinute10("minute10", minute10);
+  setMinute01("minute01", minute01);
   setBackgroundImage("cover", 'cover');
 });
 
