35a02bc7dc7cd17a2b44bfa629bf6ae4638869d7
Steve Geluso <stevegeluso@gmail.com>
2016-12-23 21:16:51 -0800

    clock shows proper time

--- main.js
+++ main.js
@@ -9,10 +9,45 @@ function showRandomTime() {
   var hour = Math.floor(Math.random() * 12) + 1;
   var minute = Math.floor(Math.random() * 60);
 
-  var hour10 = Math.floor(hour / 10);
-  var hour01 = hour % 10;
-  var minute10 = Math.floor(minute / 10);
-  var minute01 = minute % 10;
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  if (hours > 12) {
+    hours -= 12;
+  }
+
+  console.log(hours, minutes, seconds);
+
+  var hour10 = Math.floor(hours / 10);
+  var hour01 = hours % 10;
+  var minute10 = Math.floor(minutes / 10);
+  var minute01 = minutes % 10;
+  var seconds10 = Math.floor(seconds / 10);
+  var seconds01 = seconds % 10;
+
+  // built for parties starting after 8PM, going past 11PM, going through 6AM.
+  // the clock should show 08:30 PM
+  // the clock should show 10:30 PM
+  // the clock should show 10:59 PM
+  // the clock should show 59:59 for 11:00:01 PM
+  // the clock should show 59:58 for 11:00:02 PM
+  // the clock should show 01:00 for 11:59:00 PM
+  // the clock should show 00:59 for 11:59:01 PM
+  // the clock should show 00:10 for 11:59:50 PM
+  // the clock should show 00:00 for 12:00:00 PM 2017
+  // the clock should show 00:01 for 12:00:01 PM 2017
+  // uhh, so basically it shows military time for anything but the 11PM hour
+  if (hour10 === 1 && hour01 === 1) {
+    minutesLeft = 60 - (minute10 * 10 + minute01);
+
+    hour10 = Math.floor(minutesLeft / 10);
+    hour01 = minutesLeft % 10;
+
+    minutes10 = seconds10;
+    minutes01 = seconds01;
+  }
 
   console.log(hour10, hour01, minute10, minute01);
 
@@ -34,18 +69,18 @@ function setPrefix(id, image) {
   setBackgroundImage(id, 'digit_' + image);
 }
 
-function setHour10(id, image) {
+function setMinute01(id, image) {
   setPrefix(id, image + "");
 }
 
-function setHour01(id, image) {
+function setMinute10(id, image) {
   setPrefix(id, image + "-001");
 }
 
-function setMinute10(id, image) {
+function setHour01(id, image) {
   setPrefix(id, image + "-002");
 }
 
-function setMinute01(id, image) {
+function setHour10(id, image) {
   setPrefix(id, image + "-003");
 }
